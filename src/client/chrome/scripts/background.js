b46e682a8bfd3b0908c8c2b06a213e4e3851344e
gracemiller <grace.a.miller.17@dartmouth.edu>
2016-10-29 14:08:05 -0400

    Grace edited the background script to take in the new page updated, and fire a post to the backend at the right time.  As well got the new api calls linked, and created an event listener for a tab removed.

--- src/client/chrome/scripts/background.js
+++ src/client/chrome/scripts/background.js
@@ -1,27 +1,38 @@
 
-
+//listens when a tab is opened, page is visited
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   console.log("----ON UPDATED----")
   console.log("CHANGEINFO");
   console.log(changeInfo);
   console.log("TAB");
   console.log(tab);
-  if(tab.status == 'complete' && changeInfo.title){
-    console.log("-------PUTTING IN---------");
+  var domain_reg = /.*([^\.]+)(com|net|org|info|coop|int|co\.uk|org\.uk|ac\.uk|uk)$/;
+  var domain = domain_reg.exec(tab.url);
+  domain = "test";
+  console.log(domain);
+  if(changeInfo.status == 'complete' && tab.title){
+      console.log("-------PUTTING IN---------");
 
-    fetch('http://127.0.0.1:8000/pages/', {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: JSON.stringify({title: tab.title, url: tab.url})
+      fetch('http://127.0.0.1:8000/newpage/', {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({"tab":tab.id, "title":tab.title, "domain":domain, "url":tab.url}
+  )
+
     });
-  }else{
-    // console.log("else");
   }
 
 });
+
+//listens when tab is removed
+chrome.tabs.onRemoved.addListener(function( tabId,  removeInfo) {
+  console.log("tab removed!");
+
+});
+
 
 chrome.tabs.onActivated.addListener(function (activeInfo){
   console.log("onActivated");
