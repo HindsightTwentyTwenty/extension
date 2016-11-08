a627df86d9b562f00ee9f01c5148061e44e04b19
Tommy Kiernan <tommykiernan@tommys-mbp.wireless.dartmouth.edu>
2016-11-08 12:28:58 -0500

    adding favIconUrl to newTab

--- src/client/chrome/scripts/background.js
+++ src/client/chrome/scripts/background.js
@@ -1,15 +1,20 @@
 //listens when a tab is opened, page is visited
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   var domain = tab.url.replace('http://','').replace('https://','').split(/[/?#]/)[0];
+  console.log('-------');
+  console.log(changeInfo);
+  console.log(tab);
   if(changeInfo.status == 'complete' && tab.title){
+    console.log('+++++++++++')
       if(tab.url != 'chrome://newtab/'){
+
         fetch('http://127.0.0.1:8000/newpage/', {
           headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
           method: "POST",
-          body: JSON.stringify({"tab":tab.id, "title":tab.title, "domain":domain, "url":tab.url})
+          body: JSON.stringify({"tab":tab.id, "title":tab.title, "domain":domain, "url":tab.url, "favIconUrl":tab.favIconUrl})
         }
       );
     }
@@ -32,6 +37,7 @@ chrome.tabs.onRemoved.addListener(function( tabId, removeInfo) {
 
 
 chrome.tabs.onActivated.addListener(function (activeInfo){
+  console.log('on actived');
   chrome.windows.get(activeInfo.windowId, function (window) {
     fetch('http://127.0.0.1:8000/active/', {
       headers: {
