36185e477703996a12ebedb01e6d04fd02ba3613
Thomas Kiernan <tommykiernan@gmail.com>
2017-01-15 15:31:52 -0500

    Small change from Object.Assigns to spread operators in User Reducer for better readability #112

--- src/client/app/reducers/UserReducer.js
+++ src/client/app/reducers/UserReducer.js
@@ -7,27 +7,21 @@ function userReducer(state = {user_name:"", token:"", forgot:false, invalid_logi
   switch(action.type){
     case types.RECEIVE_USER_TOKEN:
       chrome.storage.local.set({"hindsite-token": action.token});
-      return Object.assign({}, {user_name:action.user_name, token:action.token, forgot: false, invalid_login:false, create_user: false, change_password: PasswordConstants.Closed});
+      return { ...state, user_name:action.user_name, token:action.token, forgot: false, invalid_login:false, create_user: false }
     case types.USER_ERROR:
-      return Object.assign({}, {user_name: state.user_name, token:state.token, forgot: state.forgot, invalid_login:true, create_user: false, change_password: PasswordConstants.Closed});
+      return { ...state, invalid_login:true, create_user: false }
     case types.RECEIVE_USER_TOKEN_FROM_CHROME:
       if(action.token != null && action.token['hindsite-token'] != null){
-        return Object.assign({}, {user_name: state.user_name, token:action.token['hindsite-token'], forgot: false, invalid_login:false, create_user: false, change_password: PasswordConstants.Closed});
+        return { ...state, token:action.token['hindsite-token'], forgot: false, invalid_login:false, create_user: false }
       } else {
         return state;
       }
     case types.FORGOT_MY_PASSWORD_PAGE:
-      return Object.assign({}, {user_name: state.user_name, token:state.token, forgot: action.forgot, invalid_login:false, create_user: false, change_password: PasswordConstants.Closed});
+      return { ...state, forgot: action.forgot, invalid_login:false, create_user: false }
     case types.CREATE_NEW_USER:
-      return Object.assign({}, {user_name: state.user_name, token:state.token, forgot: state.forgot, invalid_login:false, create_user: action.create_user, change_password: PasswordConstants.Closed});
-      // return {...state, create_user: action.create_user}
+      return { ...state, invalid_login:false, create_user: action.create_user }
     case types.CHANGE_PASSWORD:
       return { ...state, change_password: action.change_password }
-    case types.TEST:
-      console.log("TEST IN REDUCER");
-      return state;
-
-      //return [...state, forgot=action.forgot];
     default:
         return state;
   }
