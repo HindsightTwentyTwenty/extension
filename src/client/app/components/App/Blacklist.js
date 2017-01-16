32d8133082ea831810263a4cf7fd8ee5310998ad
Wanda Czerwinski <wanda.czerwinski@gmail.com>
2017-01-15 19:56:00 -0500

    add alert to categories page

--- src/client/app/components/App/Blacklist.js
+++ src/client/app/components/App/Blacklist.js
@@ -21,7 +21,6 @@ class Blacklist extends Component {
       </div>
     )
   }
-
 }
 
 export default connect(null, null)(Blacklist);
--- src/client/app/reducers/BlacklistReducer.js
+++ src/client/app/reducers/BlacklistReducer.js
@@ -1,3 +1,10 @@
 import * as types from '../constants/ActionTypes';
 
+function blacklistReducer(state = { sites: [], blacklistTextEntry: ""}, action){
+  switch(action.type){
+    default:
+        return state;
+  }
+}
+
 export default blacklistReducer;
