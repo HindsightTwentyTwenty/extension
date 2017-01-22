8c0775d0e464edfef6926d705fcaad0eee8d0d80
sgarg17 <shelley.garg.17@dartmouth.edu>
2017-01-22 14:19:54 -0500

    css cleanup

--- src/client/app/components/App/DisplayDetails.js
+++ src/client/app/components/App/DisplayDetails.js
@@ -34,7 +34,7 @@ class DisplayDetails extends Component {
           <div className="row">
             <p>visited: <Timestamp time={this.props.displayPage.visited} format="full"/></p>
           </div>
-          <div className="popup-main-form display-view">
+          <div className="display-view">
             <CategoryEntry/>
             {categories}
           </div>
