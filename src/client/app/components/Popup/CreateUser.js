708f098440961cc7b09347e531e1a1a296b268e7
Wanda Czerwinski <wanda.czerwinski@gmail.com>
2017-01-22 12:54:49 -0500

    fix css of create user

--- src/client/app/components/Popup/CreateUser.js
+++ src/client/app/components/Popup/CreateUser.js
@@ -5,7 +5,6 @@ import {render} from 'react-dom';
 import * as UserActions from '../../actions/User/UserActions.js';
 import * as PopupConstants from '../../constants/PopupConstants.js';
 
-
 function getState() {
 	return {
     email: "",
@@ -26,10 +25,8 @@ class CreateUser extends Component {
   }
 
   createNewUser(){
-    console.log(this.state);
     this.props.user_actions.createNewUser(this.state.email, this.state.password_1, this.state.password_2, this.state.first_name, this.state.last_name);
     this.props.user_actions.updatePopupStatus(PopupConstants.SignIn);
-
   }
 
 	back() {
@@ -37,42 +34,28 @@ class CreateUser extends Component {
 	}
 
   render () {
-
     return (
-      <div>
-				<button className="btn btn-primary" type="button" onClick={() => {
-					this.back();
-				}}>Back</button>
+			<div className="popup-main-form">
 				<br/>
-        <span>Create a new user account:</span>
-        <div className ="row">
-          <div className="col-xs-12">
-            <div className="input-group">
-              <input type="text" className="form-control" id="first_name" placeholder="First Name" onChange={this.updateField.bind(this)} />
-              <input type="text" className="form-control" id="last_name" placeholder="Last Name" onChange={this.updateField.bind(this)} />
-
-              <input type="email" className="form-control" id="email" placeholder="email" onChange={this.updateField.bind(this)} />
-              <input type="password" className="form-control" id="password_1" placeholder="password" onChange={this.updateField.bind(this)} />
-              <input type="password" className="form-control" id="password_2" placeholder="confirm password" onChange={this.updateField.bind(this)} />
-
-            </div>
-            <br/>
-            <span className="input-group-btn">
-            <button className="btn btn-primary canteloupe add-category-btn" type="button" onClick={this.createNewUser.bind(this)}>Submit</button>
-            </span>
-          </div>
+        <p>Create a new user account:</p>
+				<div className = 'popup-form-group'>
+					<input type="text" className="popup-form form-control" id="first_name" placeholder="first name" onChange={this.updateField.bind(this)} />
+					<input type="text" className="popup-form form-control" id="last_name" placeholder="last name" onChange={this.updateField.bind(this)} />
+					<input type="email" className="popup-form form-control" id="email" placeholder="email" onChange={this.updateField.bind(this)} />
+					<input type="password" className="popup-form form-control" id="password_1" placeholder="password" onChange={this.updateField.bind(this)} />
+					<input type="password" className="popup-form form-control" id="password_2" placeholder="confirm password" onChange={this.updateField.bind(this)} />
+        </div>
+				<div className ="popup-button-group">
+          <button className="btn btn-primary canteloupe" type="button" onClick={this.createNewUser.bind(this)}>Submit</button>
+          <button className="btn btn-primary" type="button" onClick={() => {this.back()}}>Back</button>
         </div>
       </div>
     )
   }
 }
 
-let mapStateToProps = (state) => ({
-
-})
-
 let mapDispatchToProps = (dispatch) => ({
   user_actions: bindActionCreators(UserActions, dispatch)
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(CreateUser);
+export default connect(null, mapDispatchToProps)(CreateUser);
