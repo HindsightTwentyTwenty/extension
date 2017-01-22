c923afc867079032be9da03b6b2850c49162c265
Wanda Czerwinski <wanda.czerwinski@gmail.com>
2017-01-22 12:01:42 -0500

    clean nocontent.js

--- src/client/app/components/Popup/NoContent.js
+++ src/client/app/components/Popup/NoContent.js
@@ -3,7 +3,6 @@ import {connect} from 'react-redux';
 import { bindActionCreators} from 'redux';
 import {render} from 'react-dom';
 
-
 class NoContent extends Component {
 
   constructor(props) {
@@ -12,20 +11,11 @@ class NoContent extends Component {
 
   render () {
     return (
-      <div className="container popup-body">
-        <div className="error-message">
-          <h4> Please navigate to a different page to use hindsite.</h4>
-        </div>
+      <div className="error-message">
+        <h4> Please navigate to a different page to use hindsite.</h4>
       </div>
     )
   }
 }
 
-let mapStateToProps = (state) => ({
-
-})
-
-let mapDispatchToProps = (dispatch) => ({
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(NoContent);
+export default connect(null, null)(NoContent);
