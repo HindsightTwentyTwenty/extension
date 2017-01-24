769cd7aa94c509a554dc2e406844cb1402390b39
sgarg17 <shelley.garg.17@dartmouth.edu>
2017-01-23 23:58:44 -0500

    don't post if not logged in

--- src/client/chrome/scripts/background.js
+++ src/client/chrome/scripts/background.js
@@ -12,14 +12,13 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 
 //listens when a tab is opened, page is visited
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  var domain = tab.url.replace('http://','').replace('https://','').split(/[/?#]/)[0];
-  closed = false;
-  if(changeInfo.status == 'complete' && tab.title){
+  if(token && changeInfo.status == 'complete' && tab.title){
       chrome.tabs.sendMessage(tab.id, {text: 'get_dom'}, function(dom){
         var lastError = chrome.runtime.lastError;
         if (lastError) {
           var dom = "";
         }
+        var domain = tab.url.replace('http://','').replace('https://','').split(/[/?#]/)[0];
         if(tab.url != 'chrome://newtab/'){
           fetch('https://hindsite2020.herokuapp.com/newpage/', {
             headers: {
@@ -33,43 +32,48 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         );
       }
     });
+    closed = false;
   }
 });
 
 //listens when tab is removed
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-  closed = true;
-  fetch('https://hindsite2020.herokuapp.com/closetab/', {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': "Token " + token
-    },
-    method: "POST",
-    body: JSON.stringify({"tab": tabId})
-  });
+  if(token){
+    closed = true;
+    fetch('https://hindsite2020.herokuapp.com/closetab/', {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': "Token " + token
+      },
+      method: "POST",
+      body: JSON.stringify({"tab": tabId})
+    });
+  }
 });
 
 chrome.tabs.onActivated.addListener(function (activeInfo){
-  chrome.tabs.get(activeInfo.tabId, function (tab){
-    var domain = tab.url.replace('http://','').replace('https://','').split(/[/?#]/)[0];
-    chrome.tabs.sendMessage(tab.id, {text: 'get_dom'}, function(dom){
-      var lastError = chrome.runtime.lastError;
-      if (lastError) {
-        var dom = "";
-      }
-      if(tab.url != 'chrome://newtab/'){
-        fetch('https://hindsite2020.herokuapp.com/active/', {
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': "Token " + token
-          },
-          method: "POST",
-          body: JSON.stringify({"tab": activeInfo.tabId, "closed": closed, "title":tab.title, "domain":domain, "url":tab.url, "favIconUrl":tab.favIconUrl, "previousTabId": tab.openerTabId, "active": tab.active, "html": dom})
-        });
-      }
-      closed = false;
+  if(token){
+    chrome.tabs.get(activeInfo.tabId, function (tab){
+      chrome.tabs.sendMessage(tab.id, {text: 'get_dom'}, function(dom){
+        var lastError = chrome.runtime.lastError;
+        if (lastError) {
+          var dom = "";
+        }
+        var domain = tab.url.replace('http://','').replace('https://','').split(/[/?#]/)[0];
+        if(tab.url != 'chrome://newtab/'){
+          fetch('https://hindsite2020.herokuapp.com/active/', {
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': "Token " + token
+            },
+            method: "POST",
+            body: JSON.stringify({"tab": activeInfo.tabId, "closed": closed, "title":tab.title, "domain":domain, "url":tab.url, "favIconUrl":tab.favIconUrl, "previousTabId": tab.openerTabId, "active": tab.active, "html": dom})
+          });
+        }
+        closed = false;
+      });
     });
-  });
+  }
 });
