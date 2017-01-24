30e34ff47bc97c464d9eaae3a104006826b2221c
sgarg17 <shelley.garg.17@dartmouth.edu>
2017-01-23 23:34:38 -0500

    send newPage info on /active call

--- src/client/chrome/scripts/background.js
+++ src/client/chrome/scripts/background.js
@@ -1,9 +1,7 @@
-var closed = false
-var token = ""
+var closed = false;
+var token = "";
 
 function get_token(token_return){
-  console.log("get token BACKGROUND");
-  console.log(token_return);
   token = token_return['hindsite-token'];
 }
 chrome.storage.local.get("hindsite-token", get_token);
@@ -14,12 +12,12 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 
 //listens when a tab is opened, page is visited
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  console.log("tab opened");
   var domain = tab.url.replace('http://','').replace('https://','').split(/[/?#]/)[0];
-  closed = false
+  closed = false;
   if(changeInfo.status == 'complete' && tab.title){
       chrome.tabs.sendMessage(tab.id, {text: 'get_dom'}, function(dom){
-        if(!dom){
+        var lastError = chrome.runtime.lastError;
+        if (lastError) {
           var dom = "";
         }
         if(tab.url != 'chrome://newtab/'){
@@ -28,7 +26,6 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
               'Accept': 'application/json',
               'Content-Type': 'application/json',
               'Authorization': "Token " + token
-
             },
             method: "POST",
             body: JSON.stringify({"tab":tab.id, "title":tab.title, "domain":domain, "url":tab.url, "favIconUrl":tab.favIconUrl, "previousTabId": tab.openerTabId, "active": tab.active, "html": dom})
@@ -40,36 +37,39 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 });
 
 //listens when tab is removed
-chrome.tabs.onRemoved.addListener(function( tabId, removeInfo) {
-  chrome.windows.get(removeInfo.windowId, function (window) {
-    closed = true
-    fetch('https://hindsite2020.herokuapp.com/closetab/', {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': "Token " + token
-
-      },
-      method: "POST",
-      body: JSON.stringify({"tab":tabId})
-    });
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  closed = true;
+  fetch('https://hindsite2020.herokuapp.com/closetab/', {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': "Token " + token
+    },
+    method: "POST",
+    body: JSON.stringify({"tab": tabId})
   });
 });
 
-
 chrome.tabs.onActivated.addListener(function (activeInfo){
-  console.log("tab activated");
-  chrome.windows.get(activeInfo.windowId, function (window) {
-    fetch('https://hindsite2020.herokuapp.com/active/', {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': "Token " + token
-
-      },
-      method: "POST",
-      body: JSON.stringify({"tab": activeInfo.tabId, "closed": closed})
+  chrome.tabs.get(activeInfo.tabId, function (tab){
+    var domain = tab.url.replace('http://','').replace('https://','').split(/[/?#]/)[0];
+    chrome.tabs.sendMessage(tab.id, {text: 'get_dom'}, function(dom){
+      var lastError = chrome.runtime.lastError;
+      if (lastError) {
+        var dom = "";
+      }
+      if(tab.url != 'chrome://newtab/'){
+        fetch('https://hindsite2020.herokuapp.com/active/', {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': "Token " + token
+          },
+          method: "POST",
+          body: JSON.stringify({"tab": activeInfo.tabId, "closed": closed, "title":tab.title, "domain":domain, "url":tab.url, "favIconUrl":tab.favIconUrl, "previousTabId": tab.openerTabId, "active": tab.active, "html": dom})
+        });
+      }
+      closed = false;
     });
-    closed = false
   });
 });
