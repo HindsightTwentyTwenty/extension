f4132d76604671f488943ac64d75fba49f5f060e
gracemiller <grace.a.miller.17@dartmouth.edu>
2017-02-07 11:41:30 -0500

    iframe works with srcdoc being the html from the dom.  If no html exists no modal pops up, will handle those errors at a later date.  As well, if you click the background th emodal closes

--- src/client/app/components/Bars/PageUrlBar.js
+++ src/client/app/components/Bars/PageUrlBar.js
@@ -24,7 +24,7 @@ class PageUrlBar extends Component {
 
   getCategories() {
     var _this = this;
-    this.props.category_actions.fetchCategoriesAndPages(this.props.currentUser.token); 
+    this.props.category_actions.fetchCategoriesAndPages(this.props.currentUser.token);
     return this.props.page.categories.map(function(category){
       return <div className={'url-bar-category'} key={category.title}>
           {category.title}
@@ -38,48 +38,36 @@ class PageUrlBar extends Component {
   }
 
   getDOM(){
-    console.log("token:", this.props.currentUser.token);
-    console.log("props:", this.props);
-
     this.props.lookback_actions.getDOM(this.props.visit_pk, this.props.currentUser.token);
   }
 
   openIframe(event){
-    this.getDOM();
-    // if(this.props.search.dom){
-      console.log("search item ish", this.props.search_items);
       this.setState({ iframehider_show: true });
       this.setState({ iframe_show: true });
-    // }
+      this.getDOM();
   }
 
   closeIframe(event){
     this.setState({ iframehider_show: false });
     this.setState({ iframe_show: false });
-
+    this.props.lookback_actions.clearDOM();
   }
 
   render() {
-    // <iframe className="m-iframe" src={this.props.page.url}></iframe>
-    //                <iframe className="m-iframe" srcdoc={this.props.search_items.dom}></iframe>
-
 
     var starred = this.props.page.star ? 'fa fa-star fa-2x star-categories' : 'fa fa-star-o fa-2x star-categories';
     return (
       <div className={'url-bar'}>
-        {this.state.iframe_show ?
+        {(this.props.search_items.dom && this.state.iframe_show) ?
             <div className="modal-base" id="iframe-modal">
                 <button id="iframe-close-button " onClick={this.closeIframe.bind(this)}>
                   x
                 </button>
-                <iframe className="m-iframe" src={this.props.page.url}></iframe>
+                <iframe className="m-iframe" srcDoc={this.props.search_items.dom}></iframe>
             </div>
         : ''}
-        {this.state.iframehider_show ? <div className="hider" id="iframe-hider"></div>: ''}
+        {(this.state.iframehider_show && this.props.search_items.dom ) ? <div className="hider" onClick={this.closeIframe.bind(this)} id="iframe-hider"></div>: ''}
         <a className={'url'} target="_blank" href={this.props.page.url}>{this.props.page.title}</a>
-        <button id="iframe-open-button" onClick={this.openIframe.bind(this)}>
-          <span className="glyphicon glyphicon-eye-open"></span>
-        </button>
         <div className='url-categories'>
           {this.getCategories()}
           <div onClick={()=>{
@@ -87,6 +75,9 @@ class PageUrlBar extends Component {
             }}>
             <i className={starred}></i>
           </div>
+          <button id="iframe-open-button" onClick={this.openIframe.bind(this)}>
+            <span className="glyphicon glyphicon-eye-open"></span>
+          </button>
         </div>
       </div>
     )
