9a4e6df7e49e17ca5ed6e00d492777eb3f9169f0
Thomas Kiernan <tommykiernan@gmail.com>
2017-02-14 15:49:31 -0500

    Quick revert to using pk to avoid flattenChildren error #240

--- src/client/app/components/App/Search.js
+++ src/client/app/components/App/Search.js
@@ -63,7 +63,7 @@ class Search extends Component {
   searchResults(){
     if(this.props.search.results){
       return this.props.search.results.map(function(result) {
-        return <PageUrlBar key={result.page.title} page={result.page} domain={result.domain.base_url} visited={result.visited} visit_pk={result.pk}/>
+        return <PageUrlBar key={result.page.pk} page={result.page} domain={result.domain.base_url} visited={result.visited} visit_pk={result.pk}/>
       });
     } else {
       return <div>LOADING</div>
