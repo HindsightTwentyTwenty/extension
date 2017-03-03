e8d081640f40dde43f21ed8b16daff8a47cf02ec
Wanda Czerwinski <wanda.czerwinski@gmail.com>
2017-03-03 16:15:23 -0500

    add whitesmoke background to checked categories

--- src/client/app/reducers/CurrentTabsReducer.js
+++ src/client/app/reducers/CurrentTabsReducer.js
@@ -1,14 +1,5 @@
 import * as types from '../constants/ActionTypes';
 
-
-//WC TODO: SEEMS LIKE CAN DELETE THIS TAB THING?
-const tab = (state, action) => {
-  switch (action.type) {
-    default:
-      return state
-  }
-}
-
 function currentTabsReducer(state = [], action){
   switch(action.type){
     case types.RECEIVE_TABS:
