89154b2165365853e1db9bbf540f055630cd479c
Wanda Czerwinski <wanda.czerwinski@gmail.com>
2017-03-04 14:18:18 -0500

    rearrange pageurlbar css

--- src/client/app/components/Bars/PageUrlBar.js
+++ src/client/app/components/Bars/PageUrlBar.js
@@ -2,7 +2,6 @@ import React, { PropTypes, Component } from 'react'
 import {connect} from 'react-redux';
 import { bindActionCreators} from 'redux';
 import {render} from 'react-dom';
-import Star from '../Star/Star.js';
 import * as LookbackActions from '../../actions/App/LookbackActions.js';
 import * as StarActions from '../../actions/Star/StarActions.js';
 import * as CategoryActions from '../../actions/Category/CategoryActions.js';
@@ -24,17 +23,36 @@ class PageUrlBar extends Component {
     this.state = getState();
   }
 
+  getCategoryEntry() {
+    return (
+      <div className='url-bar-category'>
+        <div className="hide-overflow">
+          <input type="text" className="url-bar-form" placeholder="add category" ref={node => {
+            this.input = node;
+          }} />
+        </div>
+        <div className='url-bar-category-button' onClick={()=> {
+          if (this.input.value.trim() !== '') {
+            this.addNewCategory(this.input.value);
+            this.input.value = '';
+          }
+        }}><i className="fa fa-plus" aria-hidden="true"></i>
+        </div>
+      </div>
+    );
+  }
+
   getCategories() {
     if (this.props.page.categories) {
       return this.props.page.categories.map((category) => {
-        return <div className={'url-bar-category'} key={category.title} style={{"backgroundColor" : category.color}}>
+        return <div className='url-bar-category-thin' key={category.title} style={{"backgroundColor" : category.color}}>
             <div className="hide-overflow">{category.title}</div>
-            <div className='url-bar-category-times' onClick={()=>{
+            <div className='url-bar-category-button' onClick={()=>{
                 this.props.category_actions.toggleCategory(this.props.page.url, category, false, this.props.currentUser.token);
               }}>
             <i className='fa fa-times'></i>
             </div>
-          </div>;
+          </div>
       });
     }
   }
@@ -76,7 +94,6 @@ class PageUrlBar extends Component {
   }
 
   render() {
-    var starred = this.props.page.star ? 'fa fa-star fa-2x star-categories starred' : 'fa fa-star-o fa-2x star-categories';
     var modal = (this.state.iframe_show) ?
         <div className="modal-base" id="iframe-modal">
           <div className="i-modal-header">
@@ -93,31 +110,35 @@ class PageUrlBar extends Component {
         </div>
     : ''
     var hider = (this.state.iframehider_show ) ? <div className="hider" onClick={this.closeIframe.bind(this)} id="iframe-hider"></div>: ''
-    var visited = this.props.visited ? <p>visited: <Timestamp time={this.props.visited} format="full"/></p> : '';
+    var visited = this.props.visited ? <p><Timestamp time={this.props.visited} format="full"/></p> : '';
     var domain = this.props.domain ? <p>{this.props.domain}</p> : '';
-
+    var starred = this.props.page.star ? 'fa fa-star fa-2x star-categories starred' :
+    'fa fa-star-o fa-2x star-categories';
     return (
       <div className="page-url-bar">
         {modal}
         {hider}
-          <div className="bar-text-col">
-            <a className="url" target="_blank" href={this.props.page.url}>{this.props.page.title}</a>
-            <div>
-              {visited}
-              {domain}
-            </div>
+        <button className="iframe-open-button" onClick={this.openIframe.bind(this)}>
+          <i className="fa fa-eye" aria-hidden="true"></i>
+        </button>
+        <div className="bar-text-col">
+          <a className="url" target="_blank" href={this.props.page.url}>{this.props.page.title}</a>
+          <div>
+            {domain}
+            {visited}
           </div>
-          <div className='url-categories vertical-center'>
-            {this.getCategories()}
-          </div>
-          <div className='url-buttons vertical-center'>
+        </div>
+        <div className='url-categories-col vertical-center'>
+          <div className='url-bar-input'>
+            {this.getCategoryEntry()}
             <div className='star-div' onClick={this.toggleStar.bind(this)}>
               <i className={starred}></i>
             </div>
-            <button className="iframe-open-button" onClick={this.openIframe.bind(this)}>
-              <i className="fa fa-eye" aria-hidden="true"></i>
-            </button>
           </div>
+          <div className='url-bar-categories'>
+            {this.getCategories()}
+          </div>
+        </div>
       </div>
     )
   }
