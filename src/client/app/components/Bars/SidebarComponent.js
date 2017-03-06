925084fd8a443e223c8fa146b466369817d5356c
Wanda Czerwinski <wanda.czerwinski@gmail.com>
2017-03-06 09:35:00 -0500

    add color picker

--- src/client/app/components/Bars/SidebarComponent.js
+++ src/client/app/components/Bars/SidebarComponent.js
@@ -4,11 +4,65 @@ import {connect} from 'react-redux';
 import { bindActionCreators} from 'redux';
 import SidebarCategoryBar from './SidebarCategoryBar';
 import * as CategoryActions from '../../actions/Category/CategoryActions.js';
+import * as GlobalConstants from '../../constants/GlobalConstants.js';
+
+function getState(){
+  return{
+    editColor: GlobalConstants.DEFAULT_CAT_COLOR.code,
+    showColorPicker: false
+  }
+}
 
 class SidebarComponent extends Component {
-
   constructor(props) {
     super(props);
+    this.state = getState();
+  }
+
+  addNewCategory(categoryTitle){
+      this.props.category_actions.pushCategory(categoryTitle, this.props.categories.editCatColor.code, this.props.currentUser.token).then(() => {
+        for (var key in this.props.categories.cats) {
+          if (categoryTitle == this.props.categories.cats[key].title) {
+            this.props.category_actions.toggleCategory(this.props.currentPage.url,
+              this.props.categories.cats[key], true, this.props.currentUser.token);
+            break;
+          }
+        }
+    });
+  }
+
+  keyPressed(event){
+    var keycode = event.keyCode || event.which;
+    if(keycode == '13') {
+      var inputValue = this.input.value.trim();
+      if (inputValue !== '') {
+        this.addNewCategory(inputValue);
+        this.input.value = '';
+      }
+    }
+  }
+
+  toggleColorPicker() {
+    this.setState({
+      showColorPicker: !this.state.showColorPicker
+    });
+  }
+
+  changeEditColor(color) {
+    this.setState({
+      editColor: color
+    });
+  }
+
+  showEditCatColorPicker() {
+    if (this.state.showColorPicker) {
+      return GlobalConstants.CAT_COLORS.map((color) => {
+        return <div className='color-square-small'
+        onClick={this.changeEditColor.bind(this, color.code)}
+        style={{"backgroundColor" : color.code}}
+        key={color.name}></div>
+      });
+    }
   }
 
   getCategories() {
@@ -29,6 +83,7 @@ class SidebarComponent extends Component {
     var categories = this.getCategories();
     var showStarredStyle = this.props.categoriesAndPages.showStarred ? {"fontWeight" : "bold"} : {};
     var starClass = this.props.categoriesAndPages.showStarred ? 'fa fa-star side-bar-single-button selected' : 'fa fa-star side-bar-single-button';
+    var editColor = {"backgroundColor" : this.state.editColor};
     return (
       <div className="side-bar-container">
         <div className="side-bar-header">
@@ -60,15 +115,20 @@ class SidebarComponent extends Component {
           </div>
           <div className='side-bar-category'>
             <div className='category-info'>
-              <i className='fa fa-plus side-bar-single-button'/>
-              <div className='category-title'
-                onClick={() => {
-
-                }}>
-                add new
+              <div className='color-square-small' style={editColor} onClick={() => {
+                this.toggleColorPicker();
+              }}/>
+              <div className='category-title'>
+                <input type="text" className="side-bar-input" placeholder="add category" onKeyPress={this.keyPressed.bind(this)} ref={node => {
+                  this.input = node;
+                }}/>
               </div>
             </div>
+            <i className='fa fa-plus side-bar-single-button'/>
           </div>
+        </div>
+        <div className='side-bar-color-picker'>
+          {this.showEditCatColorPicker()}
         </div>
         <div className="all-categories">{categories}</div>
       </div>
