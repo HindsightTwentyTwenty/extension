03cfb70d8ac0df25af8b9ecf4f2e91f483181bc4
Zachary Tannenbaum <zach@sighten.io>
2017-03-06 02:59:08 -0500

    receive html, no idea how to do reducers and stores

--- src/client/app/actions/User/IFrameActions.js
+++ src/client/app/actions/User/IFrameActions.js
@@ -6,6 +6,7 @@ import fetch from 'isomorphic-fetch'
 export function receiveDecrypted(text){
   console.log("DECRYPTED: ", text);
   return {
+    type: types.RECEIVE_DECRYPTED
   }
 }
 
@@ -14,15 +15,18 @@ export function getIframeHTML(url, md5, ekey){
   return dispatch => {
     return fetch(url, {
           headers: {
-             'Accept': 'text/plain',
-             'Content-Type': 'application/json',
-             'x-amz-server-side​-encryption​-customer-algorithm': "AES256",
-             'x-amz-server-side​-encryption​-customer-key': ekey,
-             'x-amz-server-side​-encryption​-customer-key-MD5': md5
+             'Content-Type': 'text/html',
+             'x-amz-server-side-encryption-customer-algorithm': 'AES256',
+             'x-amz-server-side-encryption-customer-key': ekey,
+             'x-amz-server-side-encryption-customer-key-MD5': md5
            },
            method: "GET"
-         })
+         }
+       )
+       .then(response => response.text())
+       .then(text => {
+         dispatch(receiveDecrypted(text))
+       })
   }
-  // .then(response => response.json())
-  //.then(json => dispatch(receiveDecrypted(reponse)))
+
 }
