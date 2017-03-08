5e4271044f4fe8f131f408f446b114745bb85fea
Wanda Czerwinski <wanda.czerwinski@gmail.com>
2017-03-08 12:44:38 -0500

    more fix

--- src/client/app/components/App/CategoriesPage.js
+++ src/client/app/components/App/CategoriesPage.js
@@ -21,9 +21,12 @@ class CategoriesPage extends Component {
   }
 
   componentDidUpdate() {
-    if (this.props.categoriesAndPages.showStarred || (this.props.currentSearchCategories.searchCats &&
+    if (this.props.categoriesAndPages.showStarred  &&
+      this.state.displayWelcomeMessage) {
+      this.setState({displayWelcomeMessage: false});
+    } else if (this.props.currentSearchCategories.searchCats &&
       this.props.currentSearchCategories.searchCats.length &&
-      this.state.displayWelcomeMessage)) {
+      this.state.displayWelcomeMessage) {
       this.setState({displayWelcomeMessage: false});
     }
   }
