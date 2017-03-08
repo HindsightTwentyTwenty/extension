e1954259ec15a72abdae8fb2b35a92d0cf16f646
Thomas Kiernan <tommykiernan@gmail.com>
2017-03-08 15:21:47 -0500

    adding title to toggleCategory for backend to create a new page when category is added to page when tracking is off

--- src/client/app/actions/Category/CategoryActions.js
+++ src/client/app/actions/Category/CategoryActions.js
@@ -67,7 +67,9 @@ export function fetchCategoriesAndPages(token){
   }
 }
 
-export function toggleCategory(pageUrl, category, addOrDelete, token){
+export function toggleCategory(pageUrl, pageTitle, category, addOrDelete, token){
+  console.log("add cat end", pageUrl, pageTitle, category, addOrDelete, token);
+  console.log("JSON SENDING", JSON.stringify({url: pageUrl, title: pageTitle, category: category.title}));
   var dispatchType = addOrDelete ? types.ADD_PAGE_CATEGORY : types.DELETE_PAGE_CATEGORY;
   return dispatch => {
     dispatch({
@@ -82,15 +84,16 @@ export function toggleCategory(pageUrl, category, addOrDelete, token){
                'Authorization': 'Token ' + token
              },
              method: "POST",
-             body: JSON.stringify({url: pageUrl, category: category.title})
+             body: JSON.stringify({url: pageUrl, title: pageTitle, category: category.title})
            }
       )
-      .then(response => response.json())
-      .then(json => dispatch({
-        type: types.REMOVE_CAT_FROM_PAGE,
-        json: json,
-        categoryTitle: category.title
-      }))
+      .then(response => console.log(response))
+      // .then(response => response.json())
+      // .then(json => dispatch({
+      //   type: types.REMOVE_CAT_FROM_PAGE,
+      //   json: json,
+      //   categoryTitle: category.title
+      // }))
   }
 }
 
--- src/client/app/components/Bars/PageUrlBar.js
+++ src/client/app/components/Bars/PageUrlBar.js
@@ -35,7 +35,7 @@ class PageUrlBar extends Component {
       this.props.category_actions.pushCategory(categoryTitle, this.state.editColor, this.props.currentUser.token).then(() => {
         for (var key in this.props.categories.cats) {
           if (categoryTitle == this.props.categories.cats[key].title) {
-            this.props.category_actions.toggleCategory(this.props.page.url,
+            this.props.category_actions.toggleCategory(this.props.page.url, this.props.currentPage.title,
               this.props.categories.cats[key], true, this.props.currentUser.token);
             break;
           }
@@ -90,7 +90,8 @@ class PageUrlBar extends Component {
         return <div className='url-bar-category-thin' key={category.title} style={{"backgroundColor" : category.color}}>
             <div className="hide-overflow">{category.title}</div>
             <div className='url-bar-category-button' onClick={()=>{
-                this.props.category_actions.toggleCategory(this.props.page.url, category, false, this.props.currentUser.token);
+                console.log("THIS PROPS PAGE TITLE", this.props.page.title);
+                this.props.category_actions.toggleCategory(this.props.page.url, this.props.page.title, category, false, this.props.currentUser.token);
               }}>
             <i className='fa fa-times'></i>
             </div>
