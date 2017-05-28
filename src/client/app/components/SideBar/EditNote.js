dd30d4a8b0a52560abc9f7e740ca40469b1597b4
gracemiller <grace.a.miller.17@dartmouth.edu>
2017-05-28 16:06:47 -0400

    notes are done, in basic form, no format editing yet

--- src/client/app/components/SideBar/EditNote.js
+++ src/client/app/components/SideBar/EditNote.js
@@ -6,13 +6,15 @@ import * as PopupActions from '../../actions/Popup/PopupActions.js'
 
 function getState(){
   return{
-    note : ""
+    note : "",
+    editing: false
   }
 }
 
 class EditNote extends Component{
   constructor(props){
     super(props);
+    this.state = getState();
     console.log("EDIT NOTE PROPS", this.props);
   }
 
@@ -24,16 +26,50 @@ class EditNote extends Component{
       this.props.currentPage.title,
       this.state.note
     );
-
   }
 
   changeNote(event){
     var new_note =  event.target.value;
     this.setState({note: new_note});
     console.log("new note", new_note);
-    // this.props.popup_actions.editNote(this.props.currentUser.token, this.props.currentPage.url, new_note);
   }
 
+  startEditingNote(){
+    if(!this.state.editing){
+      this.setState({editing: true});
+      document.getElementById("note-text-edit").style.backgroundColor = 'white';
+    }
+  }
+
+  stopEditingNote(){
+    this.setState({editing: false});
+    document.getElementById("note-text-edit").style.backgroundColor = '#fafafa';
+    this.saveNote();
+  }
+
+  cancelEdit(){
+    this.setState({editing: false});
+    document.getElementById("note-text-edit").style.backgroundColor = '#fafafa';
+    document.getElementById("note-text-edit").value = this.props.currentPage.note;
+  }
+
+  toolBar(){
+    console.log("state", this.state);
+    if(!this.state.editing){
+      return(
+        <div id = "note-text-edit-options">
+          <i className="fa fa-2x fa-pencil-square-o icon-hover" aria-hidden="true" onClick={this.startEditingNote.bind(this)}></i>
+        </div>
+      )
+    }else{
+      return(
+        <div id = "note-text-edit-options">
+          <i className="fa fa-2x fa-times icon-hover" id='icon-canceledit' aria-hidden="true" onClick={this.cancelEdit.bind(this)}></i>
+          <i className="fa fa-2x fa-check-square-o icon-hover" id='icon-saveedit' aria-hidden="true" onClick={this.stopEditingNote.bind(this)}></i>
+        </div>
+      )
+    }
+  }
 
   render(){
     return(
@@ -44,11 +80,10 @@ class EditNote extends Component{
               type="text"
               id="note-text-edit"
               defaultValue={this.props.currentPage.note}
+              onClick={this.startEditingNote.bind(this)}
               onChange={this.changeNote.bind(this)}
           />
-          <div id = "note-text-edit-options">
-            <i className="fa fa-pencil-square-o" aria-hidden="true" onClick={this.saveNote.bind(this)}></i>
-          </div>
+          {this.toolBar()}
         </div>
       </div>
     );
